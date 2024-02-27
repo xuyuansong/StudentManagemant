430193748aca733b3fcbe39a2bafdefb43e82816
xuyaunsongsong <2021006007@qq.com>
2024-02-27 11:42:53 +0800

    完善查询信息功能

--- homewindow.cpp
+++ homewindow.cpp
@@ -19,6 +19,11 @@ homeWindow::homeWindow(QWidget *parent) :
     ui->comboBox->addItem("学号");
     ui->comboBox->addItem("家乡");
     ui->comboBox->addItem("密码");
+
+    ui->tableWidget->setColumnCount(6);
+    QStringList headlist;
+    headlist<<"姓名"<<"性别"<<"年龄"<<"专业班级"<<"学号"<<"家乡";
+    ui->tableWidget->setHorizontalHeaderLabels(headlist);
 }
 
 homeWindow::~homeWindow()
@@ -54,6 +59,28 @@ void homeWindow::on_selectBtn_clicked()
 {
     QMap<QString,QString> map={{"用户名","username"},{"性别","sex"},{"年龄","yearsold"},
                                {"专业班级","zhuanyeClass"},{"学号","xuehao"},{"家乡","home"},{"密码","password"}};
+    ui->tableWidget->setRowCount(0);
+    QString selectSql="select * from student where " + map[ui->comboBox->currentText()] + "='" + ui->lineEdit->text() + "'";
+    QSqlQuery query;
+    query.exec(selectSql);
+    QStringList datalist;
+    while(query.next())
+    {
+        for (int i = 0; i < 7; ++i) {
+            datalist.append(query.value(i).toString());
+            //qDebug()<<query.value(i).toString();
+        }
+    }
+
+    int n = datalist.size()/7;
+    for (int i = 0; i < n; ++i) {
+        QStringList templist = datalist.mid(i*7,6);
+        ui->tableWidget->insertRow(ui->tableWidget->rowCount());
+        for (int col = 0; col < templist.size(); ++col) {
+            QTableWidgetItem *pItem = new QTableWidgetItem(templist[col]);
+            ui->tableWidget->setItem(ui->tableWidget->rowCount()-1,col,pItem);
+        }
+    }
 
 }
 
--- homewindow.h
+++ homewindow.h
@@ -4,6 +4,7 @@
 #include <QWidget>
 #include <QString>
 #include <QMessageBox>
+#include <QDebug>
 
 namespace Ui {
 class homeWindow;
